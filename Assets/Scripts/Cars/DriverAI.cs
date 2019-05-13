5adc61a667cfde02b77366f61de24937385abf4f
Shea Hunter <shea.hunter@gmail.com>
2019-05-13 11:17:09 -0700

    initial commit

---
+++ Assets/Scripts/Cars/DriverAI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DriverAIState
+{
+    driving,
+    stopping,
+    waiting,
+    idle
+}
+
+public class DriverAI : MonoBehaviour
+{
+    public List<Vector2> destinations;
+    public float speed;
+
+    [SerializeField]
+    private DriverAIState currentState;
+
+    private int nextDestinationIndex = 1;
+    private Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = DriverAIState.driving;
+        rb2D = GetComponent<Rigidbody2D>();
+
+        // Start at the first destination
+        transform.position = new Vector3(destinations[0].x, destinations[0].y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        var step = speed * Time.deltaTime;
+        var destination = destinations[nextDestinationIndex];
+        var newPosition = Vector2.MoveTowards(transform.position, destination, step);
+
+        rb2D.MovePosition(newPosition);
+
+        if (destination == new Vector2(transform.position.x, transform.position.y))
+        {
+            nextDestinationIndex++;
+            if (nextDestinationIndex >= destinations.Count)
+            {
+                nextDestinationIndex = 0;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.isTrigger && !collision.otherCollider.isTrigger)
+        {
+            currentState = DriverAIState.waiting;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    walking,
+    attacking,
+    interacting,
+    idle
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public PlayerState currentState;
+
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PlayerState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // is the player in an interaction
+        if (currentState == PlayerState.interacting) { return; }
+
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        if (currentState == PlayerState.walking || currentState == PlayerState.idle)
+        {
+            UpdateAnimationAndMove();
+        }
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        change.Normalize();
+
+        myRigidbody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
+    }
+}
